f3b7b0158c000ae326076a016070d8399dfe8592
yunger <hello@yunger.dev>
2022-08-20 18:57:00 -0300

    chore(useauth): add logout function

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,12 +1,18 @@
+import { signOut } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../services/firebase";
 
 export const useAuth = () => {
 	const [user, loading, error] = useAuthState(auth);
 
+	async function logout() {
+		await signOut(auth);
+	}
+
 	return {
 		user,
 		loading,
 		error,
+		logout,
 	};
 };
