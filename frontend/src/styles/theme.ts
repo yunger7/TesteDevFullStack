620b4dcd065de00e1130f928202bf1fb054f3c5f
yunger <hello@yunger.dev>
2022-08-20 14:45:06 -0300

    chore(styles): add mantine theme object

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,10 @@
+import type { MantineThemeOverride } from "@mantine/core";
+
+export const theme: MantineThemeOverride = {
+	primaryColor: "orange",
+	cursorType: "pointer",
+	defaultGradient: {
+		from: "orange",
+		to: "yellow",
+	},
+};
