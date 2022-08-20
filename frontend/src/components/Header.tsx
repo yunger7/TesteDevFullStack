e43a06b46607d7331e3330e278744b1201087696
yunger <hello@yunger.dev>
2022-08-20 18:57:45 -0300

    feat(header): add action icons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,22 @@
 import {
 	Header as MantineHeader,
 	Container,
+	Skeleton,
+	Group,
+	ActionIcon,
+	useMantineColorScheme,
 	createStyles,
 } from "@mantine/core";
+import {
+	TbSun as IconSun,
+	TbMoon as IconMoon,
+	TbBrandGithub as IconGithub,
+	TbLogout as IconLogout,
+} from "react-icons/tb";
+
 import { Logo } from "./Logo";
+
+import { useAuth } from "../hooks/useAuth";
 
 const useStyles = createStyles(theme => ({
 	header: {
@@ -26,13 +39,40 @@ const useStyles = createStyles(theme => ({
 
 export const Header = () => {
 	const { classes } = useStyles();
+	const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+	const { user, loading, logout } = useAuth();
 
 	return (
 		<MantineHeader className={classes.header} height={60} p="xs">
 			<Container className={classes.container} size="xl">
 				<Logo />
+				{loading ? (
+					<Skeleton animate height={24} width={96} />
+				) : (
+					<Group spacing="sm">
+						<ActionIcon
+							variant="default"
+							component="a"
+							href="https://github.com/yunger7/TesteDevFullStack"
+							target="_blank"
+						>
+							<IconGithub size={18} />
+						</ActionIcon>
+						<ActionIcon variant="default" onClick={() => toggleColorScheme()}>
+							{colorScheme === "dark" ? (
+								<IconSun size={18} />
+							) : (
+								<IconMoon size={18} />
+							)}
+						</ActionIcon>
+						{user && (
+							<ActionIcon variant="default" onClick={logout}>
+								<IconLogout size={18} />
+							</ActionIcon>
+						)}
+					</Group>
+				)}
 			</Container>
-			{/* TODO: If signed in, show user avatar. */}
 		</MantineHeader>
 	);
 };
