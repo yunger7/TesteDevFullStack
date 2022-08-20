d37abfd3546b016263e344872bda3777d4c405cb
yunger <hello@yunger.dev>
2022-08-20 11:59:43 -0300

    feat(ui): add dark mode support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,33 @@
 import { Outlet } from "react-router-dom";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, ColorSchemeProvider } from "@mantine/core";
+import { useLocalStorage, useHotkeys } from "@mantine/hooks";
+
+import type { ColorScheme } from "@mantine/core";
 
 export const App = () => {
+	const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+		key: "mantine-color-scheme",
+		defaultValue: "light",
+		getInitialValueInEffect: true,
+	});
+
+	const toggleColorScheme = (value?: ColorScheme) =>
+		setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+	useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
 	return (
-		<MantineProvider withGlobalStyles withNormalizeCSS>
-			<Outlet />
-		</MantineProvider>
+		<ColorSchemeProvider
+			colorScheme={colorScheme}
+			toggleColorScheme={toggleColorScheme}
+		>
+			<MantineProvider
+				theme={{ colorScheme }}
+				withGlobalStyles
+				withNormalizeCSS
+			>
+				<Outlet />
+			</MantineProvider>
+		</ColorSchemeProvider>
 	);
 };
