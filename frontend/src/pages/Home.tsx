c88ff1804f1a7ceff6e2cd62dabe032d6de65a1d
yunger <hello@yunger.dev>
2022-08-21 19:59:50 -0300

    feat(home): add AddPokemon component to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,13 @@
-import { Button, Container, Text } from "@mantine/core";
-import { TbPlus as IconAdd } from "react-icons/tb";
+import { Container, Text } from "@mantine/core";
 
 import { SubHeader } from "../components/SubHeader";
+import { AddPokemon } from "../components/AddPokemon";
 
 export const Home = () => {
 	return (
 		<>
 			<SubHeader title="Meus pokémons">
-				<Button leftIcon={<IconAdd size={18} />}>Adicionar</Button>
+				<AddPokemon />
 			</SubHeader>
 			<Container size="xl" py="xl" mt="xl">
 				<Text color="dimmed" align="center">
