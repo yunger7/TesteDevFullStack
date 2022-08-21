510076e9015a71f7c3ae28f93f3ffe5934e9b2e5
yunger <hello@yunger.dev>
2022-08-21 10:00:59 -0300

    feat(pages): add basic Home layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,21 @@
-import { Title } from "@mantine/core";
+import { Button, Container, Text } from "@mantine/core";
+import { TbPlus as IconAdd } from "react-icons/tb";
+
+import { SubHeader } from "../components/SubHeader";
 
 export const Home = () => {
-	return <Title>Home</Title>;
+	return (
+		<>
+			<SubHeader title="Meus pokémons">
+				<Button leftIcon={<IconAdd size={18} />}>Adicionar</Button>
+			</SubHeader>
+			<Container size="xl" py="xl" mt="xl">
+				<Text color="dimmed" align="center">
+					{"(っ °Д °;)っ"}
+					<br />
+					Você ainda não possui nenhum Pokémon registrado!
+				</Text>
+			</Container>
+		</>
+	);
 };
