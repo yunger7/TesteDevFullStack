0d14adfd592692c62a2f78035161bc08b496663a
yunger <hello@yunger.dev>
2022-08-22 15:19:11 -0300

    chore(types): add pokemon types

---
+++ frontend/src/types/pokemon.ts
@@ -0,0 +1,71 @@
+/* PokeAPI info */
+export type Pokemon = {
+	id: number;
+	name: string;
+	height: number;
+	weight: number;
+	sprites: {
+		back_default?: string;
+		back_female?: string;
+		back_shiny?: string;
+		back_shiny_female?: string;
+		front_default?: string;
+		front_female?: string;
+		front_shiny?: string;
+		front_shiny_female?: string;
+	};
+	stats: {
+		base_stat: number;
+		effort: number;
+		stat: { name: string; url: string };
+	}[];
+	types: { slot: number; type: { name: string; url: string } }[];
+};
+
+/* User's pokemon information */
+export type PokemonInfo = {
+	name: string;
+	nickname: string;
+	level: number;
+	nature: string;
+	hp: number;
+	attack: number;
+	defense: number;
+	sp_attack: number;
+	sp_defense: number;
+	speed: number;
+};
+
+/* Pokemon info stored in Firestore */
+export type PokemonDocument = {
+	id: number;
+	name: string;
+	nickname: string;
+	nature?: string;
+	hp: number;
+	attack: number;
+	sp_attack: number;
+	sp_defense: number;
+	defense: number;
+	speed: number;
+	level: number;
+	height: number;
+	weight: number;
+	sprites: {
+		back_default?: string;
+		back_female?: string;
+		back_shiny?: string;
+		back_shiny_female?: string;
+		front_default?: string;
+		front_female?: string;
+		front_shiny?: string;
+		front_shiny_female?: string;
+	};
+	stats: {
+		[name: string]: {
+			value: number;
+			effort: number;
+		};
+	};
+	types: string[];
+};
