6378e69fa95cfc43d0136d078f019ae5b2fba37c
yunger <hello@yunger.dev>
2022-08-22 15:15:30 -0300

    chore(types): add PokemonDocument type

--- backend/src/types/Pokemon.ts
+++ backend/src/types/Pokemon.ts
@@ -1,3 +1,4 @@
+/* PokeAPI info */
 export type Pokemon = {
 	id: number;
 	name: string;
@@ -21,6 +22,7 @@ export type Pokemon = {
 	types: { slot: number; type: { name: string; url: string } }[];
 };
 
+/* User's pokemon information */
 export type PokemonInfo = {
 	name: string;
 	nickname: string;
@@ -32,4 +34,38 @@ export type PokemonInfo = {
 	sp_attack: number;
 	sp_defense: number;
 	speed: number;
+};
+
+/* Pokemon info stored in Firestore */
+export type PokemonDocument = {
+	id: number;
+	name: string;
+	nickname: string;
+	nature?: string;
+	hp: number;
+	attack: number;
+	sp_attack: number;
+	sp_defense: number;
+	defense: number;
+	speed: number;
+	level: number;
+	height: number;
+	weight: number;
+	sprites: {
+		back_default?: string;
+		back_female?: string;
+		back_shiny?: string;
+		back_shiny_female?: string;
+		front_default?: string;
+		front_female?: string;
+		front_shiny?: string;
+		front_shiny_female?: string;
+	};
+	stats: {
+		[name: string]: {
+			value: number;
+			effort: number;
+		};
+	};
+	types: string[];
 };
